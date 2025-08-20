f2076ceea6378d365279969f27249940af4e4651
RobbyP <robinpuren@gmail.com>
2025-08-20 14:48:09 +0200

    Added list of customers

---
+++ MauiBankingExercise/ViewModels/ListOfCustomersViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiBankingExercise.Services;
+using MauiBankingExercise.Models;
+using System.Collections.ObjectModel;
+
+namespace MauiBankingExercise.ViewModels
+{
+    public class ListOfCustomersViewModel : BaseViewModel
+    {
+        private BankingSeeder _databaseSeeder;
+
+        private ObservableCollection<Customer> _customers = new ObservableCollection<Customer>();
+        private Customer? _selectedCustomer;
+
+        public Customer? SelectedCusomer
+        {
+            get { return _selectedCustomer; }
+            set { _selectedCustomer = value;
+                OnPropertyChanged(nameof(SelectedCusomer));
+            }
+        }
+
+        public ObservableCollection<Customer> TheCustomers
+        {
+            get { return _customers; }
+            set
+            {
+                _customers = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ListOfCustomersViewModel(BankingSeeder service)
+        { 
+            _
+        }
+
+    }
+}
---
+++ MauiBankingExercise/Views/ListOfCustomersView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiBankingExercise.Views;
+
+public partial class ListOfCustomersView : ContentPage
+{
+	public ListOfCustomersView()
+	{
+		InitializeComponent();
+	}
+}
